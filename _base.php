4b88348c3f4d6f412204614345504cdda3b89356
[MeowMeow323] <[samchew323@gmail.com]>
2025-03-16 00:19:12 +0800

    test

--- _base.php
+++ _base.php
@@ -17,9 +17,9 @@ $db_name = 'hushandshine';
 $db_user = 'root';
 $db_pass = '';
 // Connect to database
-$pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, [
+/*$pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-]);
+]);*/
 
 // Is GET request?
 function is_get() {
