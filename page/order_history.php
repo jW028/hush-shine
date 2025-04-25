8a61d50e8134a57d43fb1b77d039cf3aab6468ab
MeowMeow323 <samchew323@gmail.com>
2025-04-25 16:21:26 +0800

    image added

--- page/order_history.php
+++ page/order_history.php
@@ -3,7 +3,7 @@ require '../_base.php';
 
 // Check if user is logged in
 if (!isset($_SESSION['user_id'])) {
-    header("Location: ../login.php");
+    header("Location: login.php");
     exit();
 }
 
