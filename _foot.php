375c05e42c5760702dcc3f851aa988708132b442
MeowMeow323 <samchew323@gmail.com>
2025-04-25 21:53:46 +0800

    mergeed about us

--- _foot.php
+++ _foot.php
@@ -15,7 +15,6 @@
         <div class="footer-section">
             <h3>Quick Links</h3>
             <ul>
-                <li><a href="/page/aboutus.php">About Us</a></li>
                 <li><a href="/page/contact.php">Contact Us</a></li>
                 <li><a href="/page/products.php">Products</a></li>
             </ul>
@@ -35,8 +34,8 @@
             <h3>Contact Info</h3>
             <ul class="contact-info">
                 <li><i class="fas fa-map-marker-alt"></i> 123 Jalan Genting Kelang, Setapak, 53300 Kuala Lumpur</li>
-                <li><i class="fas fa-phone"></i> +60 3-4023 5555</li>
-                <li><i class="fas fa-envelope"></i> info@hushshine.com</li>
+                <li><i class="fas fa-phone"></i> +60 12-345-6789</li>
+                <li><i class="fas fa-envelope"></i> hush&shine123@gmail.com</li>
             </ul>
         </div>
     </div>
